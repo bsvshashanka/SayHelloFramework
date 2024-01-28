7b13cf801bb53abdc2abf64a8a5135a78c9b5df9
bsvshashanka <bsvshashanka03@gmail.com>
2024-01-29 01:14:03 +0530

    first commit

---
+++ HelloFramework/HelloFramework/HelloFramework.h
@@ -0,0 +1,18 @@
+//
+//  HelloFramework.h
+//  HelloFramework
+//
+//  Created by Shashanka on 29/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloFramework.
+FOUNDATION_EXPORT double HelloFrameworkVersionNumber;
+
+//! Project version string for HelloFramework.
+FOUNDATION_EXPORT const unsigned char HelloFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloFramework/PublicHeader.h>
+
+
---
+++ HelloFramework/SayHello.swift
@@ -0,0 +1,14 @@
+//
+//  SayHello.swift
+//  HelloFramework
+//
+//  Created by Shashanka on 29/01/24.
+//
+
+import Foundation
+public class Hello{
+    public init(){}
+    public static func sayHello()->String{
+        return "Hello from SayHello framework"
+    }
+}
